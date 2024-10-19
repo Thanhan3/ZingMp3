339c9ba9a9b4372ce25ae895bd76eea4efd09ba5
Thanhan3 <nguyenthanhan393@gmail.com>
2024-10-19 21:10:32 +0700

    dd

--- app/src/main/java/com/example/musicplayer/data/source/remote/RetrofitHelper.kt
+++ app/src/main/java/com/example/musicplayer/data/source/remote/RetrofitHelper.kt
@@ -4,10 +4,5 @@ import com.google.gson.Gson
 import retrofit2.Retrofit
 
 object RetrofitHelper {
-    val instance: MusicService
-        get(){
-            val baseUrl = "https://thantrieu.com"
-            return Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(Gson)
-        }
 
 }
